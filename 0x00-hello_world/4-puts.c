2dc6a9e3afa3fc33f1f4f5d8ce39cae493a95edd
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-08 16:16:05 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+* main - entry point
+* main print "Programming is like building a multilingual puzzle
+* Return: always zero
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
