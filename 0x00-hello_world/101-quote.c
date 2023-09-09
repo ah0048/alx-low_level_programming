c88484bcfaa3cb117ffd675f31c27ca361078dd4
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-09 06:39:15 -0700

    edit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - entry point
+* main print and that piece of art is useful\"
+* - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+* Return: always one
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar,2015-10-19");
+	return (1);
+}
