dba9837ac71f87683ccf798438b1f37f952afdb3
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-13 16:41:45 -0700

    edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar('+');
+		_putchar(43);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		return (0);
-		_putchar('0');
+		_putchar(48);
 	}
-	if (n < 0)
+	else
 	{
 		return (-1);
-		_putchar('-');
+		_putchar(45);
 	}
-	return (0);
 }
