7742893a57e5246bb508ab09e29272e118a97c1a
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-13 17:22:54 -0700

    6. There is no such thing as absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs -  checks for alphabetic character.
+* @n: The character to print
+* Return: abs of a num
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
