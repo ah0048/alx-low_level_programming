e5f01872213a460c43c8070455e7cf9a8d43e3e9
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-17 09:59:40 -0700

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a: The integer to print
+ * @b: The integer to print
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
