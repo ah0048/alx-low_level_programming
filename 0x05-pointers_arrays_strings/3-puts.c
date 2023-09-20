a7d811e9de250df55203ae6181a34186547d41c6
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-19 18:09:51 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts - writes the character c to stdout
+ * @str: The character to print
+ * Return: On success 1.
+ */
+void _puts(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
