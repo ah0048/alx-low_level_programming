b604018275d79d3264ce37c2cf9959d4b9c4d687
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-21 12:36:56 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - reverse
+ * @str: array
+ * Return: char array.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
