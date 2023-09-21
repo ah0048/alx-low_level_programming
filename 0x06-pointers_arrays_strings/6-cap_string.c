7ef6ec56d756ba6d113b54cd21cb59c8177af26f
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-21 15:03:33 -0700

    edit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,28 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[0] = str[0] - 32;
+			if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				i == 0)
+				str[i] -= 32;
 		}
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '"')
-		{
-			if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!')
-			{
-				if (str[i] == '?')
-				{
-				if (str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
-				{
-					if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-					{
-						str[i + 1] = str[i + 1] - 32;
-					}
-				}
-				}
-			}
-		}
+		i++;
 	}
 	return (str);
 }
