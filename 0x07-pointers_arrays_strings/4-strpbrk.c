712335790356f3bba81afa71eabcb4b021dbd389
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-27 07:34:35 -0700

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strpbrk -  function fills the first n bytes of the memory area pointed
+* @s: array
+* @accept: value
+* Return: value of *s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, tmp;
+
+	tmp = 100;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				if (i <= tmp)
+				{
+					tmp = i;
+				}
+			}
+		}
+	}
+	return (s + tmp);
+}
