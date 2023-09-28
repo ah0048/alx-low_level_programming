af2459e986d57fa2711628a6c2c5ee1d2635d84e
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-28 15:10:57 -0700

    edit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,19 +6,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\n')
-	{
-	while (*s > '\0')
-	{
-		s++;
-	}
-	}
-	_putchar(*s);
-	s--;
-	if (*s == '\n')
-	{
-		_putchar(*s);
+	if (*s == '\0')
 		return;
-	}
-	_print_rev_recursion(s);
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
