f50954629e8dc1a14635ed0cbdb78b3c0ad8905d
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-09-28 17:06:06 -0700

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * factorial - writes the character c to stdout
+ * @n: The number of factorial
+ * Return: integer.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
