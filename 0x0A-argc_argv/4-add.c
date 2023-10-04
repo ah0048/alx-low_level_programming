20a35da2fc789cf88f0118a2af5379673e086a2b
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-10-04 13:59:20 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - writes program name
+ * @argc: count
+ * @argv: pointer to strings
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
