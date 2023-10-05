18d955e6bc63db6c6eed2b5155831088a911b1ad
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-10-05 15:58:39 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - create array
+ * @width: string to copy
+ * @height: 2nd string to copy
+ * Return: null or array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **a;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	a = malloc(height * sizeof(int *));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(width * sizeof(int));
+		if (a[i] == NULL)
+		{
+		for (j = 0; j < i; j++)
+		{
+			free(a[i]);
+		}
+		free(a);
+		return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+	return (a);
+}
