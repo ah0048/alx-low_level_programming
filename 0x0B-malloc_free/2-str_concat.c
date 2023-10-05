a442de6ee451194cb74eb9d35eb2879faf844798
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-10-05 14:18:41 -0700

    edit3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,7 +31,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		size2++;
 	}
-	s = malloc((size1 + size2) * sizeof(char));
+	s = malloc((size1 + size2 - 1) * sizeof(char));
 	if (s == NULL)
 	{
 		return (NULL);
