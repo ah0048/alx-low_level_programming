2f85bf7c0e1092aca5a97cf6a1b3a0015929ee9e
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-10-06 15:57:16 -0700

    1. string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - writes the character c to stdout
+ * @s1: first string
+ * @s2: second string
+ * @n: number
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+
+	char *ptr;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc((i + n - 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (n >= j)
+	{
+		n = j;
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1 == NULL)
+			s1 = "";
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j <= n; j++)
+	{
+		if (s2 == NULL)
+			s2 = "";
+		ptr[i + j] = s2[j];
+	}
+	return (ptr);
+}
