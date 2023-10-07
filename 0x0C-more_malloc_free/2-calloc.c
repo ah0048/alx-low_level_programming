4de60347c70090f1a83bd472a94b9140ce6d3940
Ahmed Harhash <ahmedharhash0048@gmail.com>
2023-10-07 09:54:51 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - writes the character c to stdout
+ * @nmemb: first string
+ * @size: second string
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+}
