95872896c10413faaa5ce4f71e2148b664d06f41
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-23 14:10:21 -0800

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "dog.h"
+/**
+ * new_dog - initialize dog
+ * @name: pointer
+ * @age: age
+ * @owner: owner
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
