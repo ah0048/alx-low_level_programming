200913d0c20fd6cc62c9a8afa5e513e7c81927cd
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-23 11:21:25 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef pragma
+#define pragma
+/**
+ * struct dog - Short doggo
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: doggo
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
