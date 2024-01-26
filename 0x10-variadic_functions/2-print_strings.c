9f90930d33f04e8fae23a4e0c4d8af3b335bedb7
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-26 13:43:20 -0800

    edit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,25 +1,28 @@
 #include "variadic_functions.h"
 /**
- * print_numbers - prints name
+ * print_strings - prints name
  * @separator: name
  * @n: number
  * Return: sum
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = n;
+
 	va_list args;
 
-	unsigned int i = n;
+	char *str;
 
 	if (!n)
 	{
 		printf("\n");
 		return;
 	}
+
 	va_start(args, n);
 	while (i--)
 	{
-		printf("%d%s", va_arg(args, int),
+		printf("%s%s", (str = va_arg(args, char *)) ? str : "(nil)",
 				i ? (separator ? separator : "") : "\n");
 	}
 	va_end(args);
