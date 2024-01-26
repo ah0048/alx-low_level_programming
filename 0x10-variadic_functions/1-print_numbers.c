39d57a2a1fe96e15812a23c109891c614ea847f5
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-26 12:16:22 -0800

    edit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,8 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != n - 1)
 		{
 			if (separator)
-				printf("%s", separator);
-			printf(" ");
+				printf("%s ", separator);
 		}
 	}
 	printf("\n");
