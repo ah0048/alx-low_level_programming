c9a8f895d4b5c149748f066f943ddd106ea398b7
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-01-26 16:02:58 -0800

    edit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints name
+ * @format: name
+ * Return: sum
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	int i = 0;
+
+	char *str;
+
+	char current;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		current = format[i];
+			switch (current)
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%i", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					{
+					str = va_arg(args, char *);
+					printf("%s", str ? str : "(nil)");
+					}
+					break;
+				default:
+					i++;
+					continue;
+			}
+		if (format[i])
+			printf(", ");
+	}
+	printf("\n");
+	va_end(args);
+}
