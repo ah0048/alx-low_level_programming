e94ac2909c55e08a559d83dcaa2f9e73145e8fe5
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-01 16:33:43 -0800

    edit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_list - writes the character c to stdout
+ * @head: The character to print
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *newnode;
+
+	if (!head)
+		return;
+	node = head;
+	while (node)
+	{
+		newnode = node->next;
+		free(node);
+		free(node->str);
+		node = newnode;
+	}
+}
