58856c4acc2d3941527935683510b8a815c7d494
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-01 16:03:12 -0800

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node - writes the character c to stdout
+ * @head: The character to print
+ * @str: string
+ * Return: num
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newhead = malloc(sizeof(list_t));
+
+	if (!head || !newhead)
+	{
+		return (NULL);
+	}
+	if (str)
+	{
+		newhead->str = strdup(str);
+		if (!newhead->str)
+		{
+			free(newhead);
+			return (NULL);
+		}
+		newhead->len = strlen(str);
+	}
+	newhead->next = *head;
+	*head = newhead->next;
+	return (newhead);
+}
