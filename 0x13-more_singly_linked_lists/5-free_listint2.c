8ec8b236fdeda727a1800e427b659f9460669469
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-06 14:55:07 -0800

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - writes the character c to stdout
+ * @head: The character to print
+ * Return: num
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *newnode;
+
+	if (!head)
+		return;
+
+	node = head;
+	while (node)
+	{
+		newnode = node->next;
+		free(node);
+		node = newnode;
+	}
+	*head = NULL;
+}
