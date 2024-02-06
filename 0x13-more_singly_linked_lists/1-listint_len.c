62832da60711e3a7958c43fbdb91c8ca92a48ce6
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-06 13:02:07 -0800

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - writes the character c to stdout
+ * @h: The head
+ *
+ * Return: num
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+	}
+	return (num);
+}
