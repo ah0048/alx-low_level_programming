d87aea4aa919cf9a7bcf4547653161b1953f9001
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-07 14:24:58 -0800

    7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - writes the character c to stdout
+ * @head: The character to print
+ * @index: index
+ * Return: num
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	listint_t *node;
+
+	if (!head)
+		return (NULL);
+	node = head;
+	for (i = 0; i < index; i++)
+	{
+		node = node->next;
+	}
+	return (node);
+}
