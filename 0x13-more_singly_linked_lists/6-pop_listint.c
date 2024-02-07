cc3a0a772f4c3479aae6eec0af9c23a77294982b
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-07 13:30:46 -0800

    6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - writes the character c to stdout
+ * @head: The character to print
+ * Return: num
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+
+	int n;
+
+	if (!head || !*head)
+	{
+		return (NULL);
+	}
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
