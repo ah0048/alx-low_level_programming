4c2ca59b1771e8e5b9cc6abfe90afd4586c9dc52
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-12 16:13:40 -0800

    edit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - read from a file
+ * @filename: pointer
+ * @text_content: index
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	size_t bytes, len = strlen(text_content);
+
+	if (!filename || !text_content)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREATE | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #define READ_BUF_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
