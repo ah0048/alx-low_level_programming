edc789b37913a3e567e2773735ff7dd6d0bca9e5
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-13 13:40:55 -0800

    2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strlen - read from a file
+ * @s: pointer
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+/**
+ * append_text_to_file - read from a file
+ * @filename: pointer
+ * @text_content: index
+ * Return: int
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	size_t bytes, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
