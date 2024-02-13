8ab354710bd48ece011dbc8395af71db3f7b3500
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-12 16:20:49 -0800

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	size_t bytes, len = strlen(text_content);
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
