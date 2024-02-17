d6b2fba634850ae4315dacf9b45e954afd19b3ea
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-02-17 12:06:34 -0800

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int ac, char **av)
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
