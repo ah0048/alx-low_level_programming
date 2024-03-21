f4ccb5b758432bf1d35bc7759ca25b81757bda40
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-03-21 16:06:03 +0200

    edit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* dlistint_len - counts all elements of a double linked list
+* @h: name of env var
+* Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+
+	size_t num = 0;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		num += 1;
+	}
+	return (num);
+}
