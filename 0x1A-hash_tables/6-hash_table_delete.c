3ff94ccbd0c8b8b9534970fbdd123fbbab6ed58a
root <root@DESKTOP-2RRMLQV.localdomain>
2024-04-13 04:12:58 +0200

    edit

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete hash table
+ * @ht: the hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	hash_node_t *current, *tmp;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			tmp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
