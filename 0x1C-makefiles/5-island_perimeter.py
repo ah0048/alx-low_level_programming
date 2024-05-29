4c739f9fd7815bdf20adf7352fea2a66598a557c
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-05-30 01:51:50 +0300

    edit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,19 +8,17 @@ def island_perimeter(grid):
     '''
     grid function
     '''
-    num = 0
-    limit = 100
-    flag_row = 1
-    flag_element = 1
-    for row in grid:
-        if flag_row > limit:
-            break
-        for element in row:
-            if flag_element > limit:
-                break
-            if element == 1:
-                num = num + 1
-            flag_element = flag_element + 1
-        flag_row = flag_row + 1
-    perimeter = 2 * num + 2
+    perimeter = 0
+    rows = len(grid)
+    columns = len(grid[0])
+    i = 0
+    j = 0
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if grid[i][j - 1] == 1:
+                    perimeter -= 2
+                if grid[i - 1][j] == 1:
+                    perimeter -= 2
     return perimeter
